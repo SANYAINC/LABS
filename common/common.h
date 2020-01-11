cf30e812517c8fa74809d3156cda072404ceb44d
SANYAINC <sanyaincorp@gmail.com>
2020-01-11 23:39:17 +0300

    Added "showArray" function.

--- common/common.h
+++ common/common.h
@@ -1,14 +1,25 @@
+#include "iostream"
+using namespace std;
+
 #ifndef LABSGIT_COMMON_H
 #define LABSGIT_COMMON_H
 
 
-template<class T>
+template <class T>
 T* createArray(int size) {
     T *array = new T[size];
     for (int i = 0; i < size; ++i) {
         array[i] = static_cast<T>(0);
     }
     return array;
+}
+
+template <class T>
+void showArray(T *array, int size) {
+    for (int i = 0; i < size; ++i) {
+        cout << array[i] << ' ';
+    }
+    cout << endl;
 }
 
 
