a1a673c749fecf334648636456205fe1ac25f6f8
SANYAINC <sanyaincorp@gmail.com>
2020-01-11 23:00:29 +0300

    Initial commit

---
+++ 5.4/5.4.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "5.4.h"
+
+int main() {
+
+    return 0;
+}
---
+++ common/common.cpp
@@ -0,0 +1,6 @@
+
+template<class T>
+T* createArray(int size) {
+    T *array = new T[size];
+    return array;
+}
