9711e09334c878db801932bbe2088c122fd11a34
SANYAINC <sanyaincorp@gmail.com>
2020-01-11 23:33:23 +0300

    Reference fixed.

--- 5.4/5.4.h
+++ 5.4/5.4.h
@@ -10,7 +10,7 @@ int isInArray(int num, const int *array, int size) {
     return -1;
 }
 
-int refMaxFib(const int *array, int size) {
+int &refMaxFib(int *array, int size) {
     int max = array[0];
     int maxIndex = 0;
     for (int i = 0; i < size; ++i) {
