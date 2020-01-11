c8a55f691d68a43b90fbdd3e18f537995b41298c
SANYAINC <sanyaincorp@gmail.com>
2020-01-11 23:33:56 +0300

    STD and test added.

--- 5.4/5.4.cpp
+++ 5.4/5.4.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
 #include "../common/common.h"
 #include "5.4.h"
+using namespace std;
 
 int main() {
-
+    int *ar = createArray<int>(5);
+    ar[0] = -5;
+    ar[1] = 1;
+    ar[2] = 150;
+    ar[3] = 0;
+    ar[4] = 55;
+    cout << refMaxFib(ar, 5);
 
     return 0;
 }
