d89b04dfe6b0fe7025dc0232ce8881fe2e9da7a3
SANYAINC <sanyaincorp@gmail.com>
2020-01-14 02:33:47 +0300

    Prototypes removed Prints added Checks added

--- 5.5/5.5.h
+++ 5.5/5.5.h
@@ -2,18 +2,27 @@
 #include <iomanip>
 using namespace std;
 
-double function(char type, double x);
-double function(char type, double x, double param);
-void solveNewton(double (*funcPtr)(char, double), double epsilon = 1E-6, double inf = 1, double sup = 4);
-void solveNewton(double (*funcPtr)(char, double, double), double epsilon = 1E-6, double inf = 0, double sup = 1.5, double paramInf = 0.95, double paramSup = 1.2, double deltaParam = 0.05);
-void printSolution(double (*funcPtr)(char, double), double xi, int iterations);
-void printSolution(double (*funcPtr)(char, double, double), double xi, double paramInf, double deltaParam, int i, int iterations);
-void printTableHeader();
 
 
+void printSolution(double (*funcPtr)(char, double), double xi, int iterations) {
+    cout << "\n";
+    cout << setw(10) << "X" << setw(20) <<       "F(x)"          << setw(20) << "Iterations\n\n";
+    cout << setw(10) <<  xi << setw(20) << (*funcPtr)('F', xi)   << setw(20) <<    iterations;
+    cout << "\n";
+}
+void printSolution(double (*funcPtr)(char, double, double), double xi, double param, int iterations) {
+    cout << setw(10) << param << setw(20) << xi << setw(20) << (*funcPtr)('F', xi, param) << setw(20) << iterations << endl;
+}
+void printTableHeader() {
+    cout << '\n';
+    cout << setw(10) << "S" << setw(20) << "X" << setw(20) << "F(x)" << setw(20) << "Iterations\n\n";
+    cout << '\n';
+}
 
 
-
+bool argsCorrect(double &inf, double &sup, double &x0) {
+    return inf <= sup and inf <= x0 and sup >= x0;
+}
 
 double function(char type, double x) {
     switch (type) {
@@ -31,7 +40,6 @@ double function(char type, double x) {
         }
     }
 }
-
 double function(char type, double x, double param) {
     switch (type) {
         case 'F': {
@@ -49,69 +57,65 @@ double function(char type, double x, double param) {
     }
 }
 
-void solveNewton(double (*funcPtr)(char, double), double epsilon, double inf, double sup) {
-    double xi = (inf + sup)/2;
+void solveNewton(double (*funcPtr)(char, double), double inf = 1, double sup = 4, double x0 = 2, double epsilon = 1E-6) {
+    if (!argsCorrect(inf, sup, x0)) {
+        cout << "INCORRECT ARGUMENTS";
+        return;
+    }
+    if ((*funcPtr)('1', x0) < epsilon) {
+        cout << "INCORRECT X0";
+        return;
+    }
+    double xi = x0;
     int iterations = 0;
     while (abs((*funcPtr)('F', xi)) > epsilon) {
         xi = xi - (*funcPtr)('F', xi)/(*funcPtr)('1', xi);
         iterations++;
-        if (xi < inf or xi > sup) {
-            cout << "NO ROOTS DETECTED";
-            return;
-        }
+    }
+    if (xi < inf or xi > sup) {
+        cout << "NO ROOTS DETECTED";
+        return;
     }
     printSolution(funcPtr, xi, iterations);
 }
-
-void solveNewton(double (*funcPtr)(char, double, double), double epsilon, double inf, double sup, double paramInf, double paramSup, double deltaParam) {
-    //TODO ВЫделить память для параметра, поменять место epsilon
+void solveNewton(double (*funcPtr)(char, double, double), double inf = 0, double sup = 1.5, double x0 = 0.2, double epsilon = 1E-6, double paramInf = 0.95, double paramSup = 1.2, double deltaParam = 0.05) {
+    if (!argsCorrect(inf, sup, x0)) {
+        cout << "INCORRECT ARGUMENTS";
+        return;
+    }
     printTableHeader();
     int n = ceil((paramSup - paramInf) / deltaParam);
-    double xi = 0.1;
+    double xi = x0;
     double xPrev;
     double xCur;
     bool isTrapped = false;
     bool outOfRange = false;
     int iterations = 0;
     for (int i = 0; i <= n; ++i) {
-        while (abs((*funcPtr)('F', xi, paramInf + deltaParam * i)) > epsilon) {
+        double param = paramInf + deltaParam * i;
+        while (abs((*funcPtr)('F', xi, param)) > epsilon) {
             xPrev = xi;
-            xi = xi - (*funcPtr)('F', xi, paramInf + deltaParam * i) / (*funcPtr)('1', xi, paramInf + deltaParam * i);
+            xi = xi - (*funcPtr)('F', xi, param) / (*funcPtr)('1', xi, param);
             iterations++;
             xCur = xi;
-            if (xi < inf or xi > sup) {
-                cout << "NO ROOTS DETECTED IN THIS RANGE\n";
-                outOfRange = true;
-                break;
-            }
-            if (abs((*funcPtr)('F', xCur, paramInf + deltaParam * i)) > abs((*funcPtr)('F', xPrev, paramInf + deltaParam * i))) {
-                cout << "EXTREMUM TRAP\n";
+            if (abs((*funcPtr)('F', xCur, param)) > abs((*funcPtr)('F', xPrev, param))) {
+                cout << "CRITICAL POINT TRAP\n";
                 isTrapped = true;
                 break;
             }
         }
-        if (!isTrapped and !outOfRange) {
-            printSolution(funcPtr, xi, paramInf, deltaParam, i, iterations);
+        if (xi < inf or xi > sup) {
+            cout << "NO ROOTS DETECTED IN THIS RANGE\n";
+            outOfRange = true;
+            break;
         }
-        xi = 0.1;
+        if (!isTrapped and !outOfRange) {
+            printSolution(funcPtr, xi, param, iterations);
+        }
+        xi = x0;
         isTrapped = false;
         outOfRange = false;
     }
 }
 
-
-void printSolution(double (*funcPtr)(char, double), double xi, int iterations) {
-    cout << "\n";
-    cout << setw(10) << "X" << setw(20) <<       "F(x)"          << setw(20) << "Iterations\n\n";
-    cout << setw(10) <<  xi << setw(20) << (*funcPtr)('F', xi)   << setw(20) <<    iterations;
-    cout << "\n";
-}
-void printSolution(double (*funcPtr)(char, double, double), double xi, double paramInf, double deltaParam, int i, int iterations) {
-    cout << setw(10) << paramInf + deltaParam * i << setw(20) << xi << setw(20) << (*funcPtr)('F', xi, paramInf + deltaParam * i) << setw(20) << iterations << endl;
-}
-void printTableHeader() {
-    cout << '\n';
-    cout << setw(10) << "S" << setw(20) << "X" << setw(20) << "F(x)" << setw(20) << "Iterations\n\n";
-    cout << '\n';
-}
 
