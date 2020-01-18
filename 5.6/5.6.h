cf9a6f0cb61d1fb63c4ee2dbc1a4c0a3cb2b5c94
SANYAINC <sanyaincorp@gmail.com>
2020-01-18 18:15:01 +0300

    Comments added

--- 5.6/5.6.h
+++ 5.6/5.6.h
@@ -8,22 +8,21 @@ void solveIntegralMR(double (*func)(double, double, double), double min = 1, dou
                      double epsilon = 1E-6,
                      double parSMin = 0, double parSMax = 0.5, double parSDelta = 0.1,
                      double parTMin = 0.5, double patTMax = 2.5, double parTDelta = 0.5);
-
+//Решить интеграл методом трапеций
 void solveIntegralT(double (*func)(double, double, double), double min = 1, double max = 2, double epsilon = 1E-6,
                     double parSMin = 0, double parSMax = 0.5, double parSDelta = 0.1,
                     double parTMin = 0.5, double patTMax = 2.5, double parTDelta = 0.5);
-
+//Решить интеграл методом Симпсона
 void solveIntegralS(double (*func)(double, double, double), double leftBorder = 1, double rightBorder = 2, double epsilon = 1E-6,
                     double parSMin = 0, double parSMax = 0.5, double parSDelta = 0.1,
                     double parTMin = 0.5, double patTMax = 2.5, double parTDelta = 0.5);
 
 
-//Посчитать интеграл с текущим количествои разбиений
+
+//Посчитать интеграл с текущим количествои разбиений соответствующим методом
 double calcIntegralMR(double (*func)(double, double, double), double leftBorder, double rightBorder, double paramS,
                       double paramT, int N);
-
 double calcIntegralT(double (*func)(double, double, double), double leftBorder, double rightBorder, double paramS,
                      double paramT, int N);
-
 double calcIntegralS(double (*func)(double, double, double), double leftBorder, double rightBorder, double paramS,
                     double paramT, int N);
