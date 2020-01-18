bfd8fa7b2de57963669426202a2f994c982eb4f3
SANYAINC <sanyaincorp@gmail.com>
2020-01-18 18:12:59 +0300

    Simpson method added

--- 5.6/5.6.cpp
+++ 5.6/5.6.cpp
@@ -8,7 +8,8 @@ double function(double x, double paramS, double paramT) {
     return ((1 + pow(x, paramT) + x * x) / (sqrt(x * x * x + 1))) * (1 - sin(paramS * x));
 }
 
-double calcIntegralMR(double (*func)(double, double, double), double leftBorder, double rightBorder, double paramS, double paramT, int N) {
+double calcIntegralMR(double (*func)(double, double, double), double leftBorder, double rightBorder, double paramS,
+                      double paramT, int N) {
     double xi = leftBorder;
     double dx = (rightBorder - leftBorder) / N;
     double integral = 0;
@@ -18,15 +19,36 @@ double calcIntegralMR(double (*func)(double, double, double), double leftBorder,
     }
     return integral * dx;
 }
-double calcIntegralT(double (*func)(double, double, double), double leftBorder, double rightBorder, double paramS, double paramT, int N) {
+
+double calcIntegralT(double (*func)(double, double, double), double leftBorder, double rightBorder, double paramS,
+                     double paramT, int N) {
     double xi = leftBorder;
     double dx = (rightBorder - leftBorder) / N;
     double integral = 0;
-    integral += ((*func)(leftBorder, paramS, paramT) + (*func)(rightBorder, paramS, paramT))/2;
+    integral += ((*func)(leftBorder, paramS, paramT) + (*func)(rightBorder, paramS, paramT)) / 2;
     for (int i = 1; i < N; ++i) {
         integral += (*func)(xi + dx * i, paramS, paramT);
     }
     return integral * dx;
+}
+
+double calcIntegralS(double (*func)(double, double, double), double leftBorder, double rightBorder, double paramS,
+                    double paramT, int N) {
+    double xi = leftBorder;
+    double dx = (rightBorder - leftBorder) / (2 * N);
+    double integral = 0;
+    for (int i = 0; i <= 2 * N; ++i) {
+        if (i == 0 or i == 2 * N) {
+            integral += (*func)(xi + dx * i, paramS, paramT);
+            continue;
+        }
+        if (i % 2 == 0) {
+            integral += 2 * (*func)(xi + dx * i, paramS, paramT);
+        } else {
+            integral += 4 * (*func)(xi + dx * i, paramS, paramT);
+        }
+    }
+    return integral * dx / 3;
 }
 
 void solveIntegralMR(double (*func)(double, double, double), double leftBorder, double rightBorder, double epsilon,
@@ -59,18 +81,19 @@ void solveIntegralMR(double (*func)(double, double, double), double leftBorder, 
                     integralN = integral2N;
                     integral2N = calcIntegralMR(func, leftBorder, rightBorder, curParS, curParT, 2 * N);
                     iterations++;
-                //Если интеграл считается в первый раз, считаем для N и для 2N
+                    //Если интеграл считается в первый раз, считаем для N и для 2N
                 } else {
                     integralN = calcIntegralMR(func, leftBorder, rightBorder, curParS, curParT, N);
                     integral2N = calcIntegralMR(func, leftBorder, rightBorder, curParS, curParT, 2 * N);
-                    iterations+=2;
+                    iterations += 2;
                 }
 
                 N *= 2;
 
             } while (abs(integralN - integral2N) > epsilon);
 
-            cout << setw(15) << curParS << setw(15) << curParT << setw(15) << integralN << setw(15) << iterations << "\n";
+            cout << setw(15) << curParS << setw(15) << curParT << setw(15) << integralN << setw(15) << iterations
+                 << "\n";
             N = 1;
             iterations = 0;
         }
@@ -79,10 +102,10 @@ void solveIntegralMR(double (*func)(double, double, double), double leftBorder, 
 
 void solveIntegralT(double (*func)(double, double, double), double leftBorder, double rightBorder, double epsilon,
                     double parSMin, double parSMax, double parSDelta,
-                    double parTMin, double patTSup, double parTDelta) {
+                    double parTMin, double patTMax, double parTDelta) {
 
     int parSIter = ceil((parSMax - parSMin) / parSDelta);
-    int parTIter = ceil((patTSup - parTMin) / parTDelta);
+    int parTIter = ceil((patTMax - parTMin) / parTDelta);
 
     double curParS;
     double curParT;
@@ -111,14 +134,64 @@ void solveIntegralT(double (*func)(double, double, double), double leftBorder, d
                 } else {
                     integralN = calcIntegralT(func, leftBorder, rightBorder, curParS, curParT, N);
                     integral2N = calcIntegralT(func, leftBorder, rightBorder, curParS, curParT, 2 * N);
-                    iterations+=2;
+                    iterations += 2;
                 }
 
                 N *= 2;
 
             } while (abs(integralN - integral2N) > epsilon);
 
-            cout << setw(15) << curParS << setw(15) << curParT << setw(15) << integralN << setw(15) << iterations << "\n";
+            cout << setw(15) << curParS << setw(15) << curParT << setw(15) << integralN << setw(15) << iterations
+                 << "\n";
+            N = 1;
+            iterations = 0;
+        }
+    }
+}
+
+void solveIntegralS(double (*func)(double, double, double), double leftBorder, double rightBorder, double epsilon,
+                    double parSMin, double parSMax, double parSDelta,
+                    double parTMin, double patTMax, double parTDelta) {
+
+    int parSIter = ceil((parSMax - parSMin) / parSDelta);
+    int parTIter = ceil((patTMax - parTMin) / parTDelta);
+
+    double curParS;
+    double curParT;
+
+    double integralN = 0;
+    double integral2N = 0;
+
+    int N = 1;
+    int iterations = 0;
+
+    cout << "\n\n";
+    cout << setw(15) << "S PARAM" << setw(15) << "T PARAM" << setw(15) << "INTEGRAL" << setw(15) << "ITERATIONS";
+    cout << "\n\n";
+
+    for (int i = 0; i <= parSIter; ++i) {
+        curParS = parSMin + parSDelta * i;
+        for (int j = 0; j <= parTIter; ++j) {
+            curParT = parTMin + parTDelta * j;
+            do {
+                //Если интеграл считается не в первый раз, ложим в N предыдущее значение из 2N, а новое 2N пересчитываем с новым параметром
+                if (integral2N != 0) {
+                    integralN = integral2N;
+                    integral2N = calcIntegralS(func, leftBorder, rightBorder, curParS, curParT, 2 * N);
+                    iterations++;
+                    //Если интеграл считается в первый раз, считаем для N и для 2N
+                } else {
+                    integralN = calcIntegralS(func, leftBorder, rightBorder, curParS, curParT, N);
+                    integral2N = calcIntegralS(func, leftBorder, rightBorder, curParS, curParT, 2 * N);
+                    iterations += 2;
+                }
+
+                N *= 2;
+
+            } while (abs(integralN - integral2N) > epsilon);
+
+            cout << setw(15) << curParS << setw(15) << curParT << setw(15) << integralN << setw(15) << iterations
+                 << "\n";
             N = 1;
             iterations = 0;
         }
--- 5.6/5.6.h
+++ 5.6/5.6.h
@@ -1,3 +1,4 @@
+#pragma once
 
 //Функция
 double function(double x, double paramS, double paramT);
@@ -12,6 +13,17 @@ void solveIntegralT(double (*func)(double, double, double), double min = 1, doub
                     double parSMin = 0, double parSMax = 0.5, double parSDelta = 0.1,
                     double parTMin = 0.5, double patTMax = 2.5, double parTDelta = 0.5);
 
+void solveIntegralS(double (*func)(double, double, double), double leftBorder = 1, double rightBorder = 2, double epsilon = 1E-6,
+                    double parSMin = 0, double parSMax = 0.5, double parSDelta = 0.1,
+                    double parTMin = 0.5, double patTMax = 2.5, double parTDelta = 0.5);
+
+
 //Посчитать интеграл с текущим количествои разбиений
 double calcIntegralMR(double (*func)(double, double, double), double leftBorder, double rightBorder, double paramS,
                       double paramT, int N);
+
+double calcIntegralT(double (*func)(double, double, double), double leftBorder, double rightBorder, double paramS,
+                     double paramT, int N);
+
+double calcIntegralS(double (*func)(double, double, double), double leftBorder, double rightBorder, double paramS,
+                    double paramT, int N);
