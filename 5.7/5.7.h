8d6d941481917b09606d730f7d0948fe9d960a35
SANYAINC <sanyaincorp@gmail.com>
2020-01-19 13:22:40 +0300

    Binary insertion sort added, sorts overloaded

--- 5.7/5.7.h
+++ 5.7/5.7.h
@@ -32,6 +32,31 @@ void quickSort(T *array, int &couCom, int &couMov, int right, int left = 0) {
         quickSort(array, couCom, couMov, right, leftPtr);
     }
 }
+template <class T>
+void quickSort(T *array, int right, int left = 0) {
+    int leftPtr = left;
+    int rightPtr = right;
+    int basis = array[(leftPtr + rightPtr) / 2];
+    while (leftPtr <= rightPtr) {
+        while (array[leftPtr] < basis) {
+            leftPtr++;
+        }
+        while (array[rightPtr] > basis) {
+            rightPtr--;
+        }
+        if (leftPtr <= rightPtr) {
+            swap(array[leftPtr], array[rightPtr]);
+            leftPtr++;
+            rightPtr--;
+        }
+    }
+    if (left < rightPtr) {
+        quickSort(array, rightPtr, left);
+    }
+    if (right > leftPtr) {
+        quickSort(array, right, leftPtr);
+    }
+}
 
 template <class T>
 void bubbleSort(T *array, int &couCom, int &couMov, int size) {
@@ -55,6 +80,26 @@ void bubbleSort(T *array, int &couCom, int &couMov, int size) {
         seria = 0;
     }
 }
+template <class T>
+void bubbleSort(T *array, int size) {
+    int index = 0;
+    int seria = 0;
+    for (int i = 0; i < size - 1; ++i) {
+        while (index + 1 < size) {
+            if (array[index] > array[index+1]) {
+                swap(array[index], array[index+1]);
+            } else {
+                seria++;
+            }
+            index++;
+        }
+        if (seria == size - 1) {
+            break;
+        }
+        index = 0;
+        seria = 0;
+    }
+}
 
 template <class T>
 void insertionSort(T *array, int &couCom, int &couMov, int size) {
@@ -64,4 +109,65 @@ void insertionSort(T *array, int &couCom, int &couMov, int size) {
             couMov++;
         }
     }
+}
+template <class T>
+void insertionSort(T *array, int size) {
+    for (int i = 1; i < size; ++i) {
+        for (int j = i; j > 0 and array[j-1] > array[j]; j--) {
+            swap(array[j], array[j-1]);
+        }
+    }
+}
+
+template <class T>
+void binInsertionSort(T *array, int &couCom, int &couMov, int size) {
+    T temp;
+    int left;
+    int right;
+    int mid;
+    for (int i = 1;  i < size; i++)
+        if (++couCom and array[i-1] > array[i]){
+            temp = array[i];
+            left = 0;
+            right = i-1;
+            do {
+                mid = (left + right) / 2;
+                if  (array[mid] < temp ) {
+                    left = mid + 1;
+                } else  {
+                    right = mid - 1;
+                }
+                couCom++;
+            } while (left <= right);
+            for (int j = i-1; j>=left; j--) {
+                array[j + 1] = array[j];
+                couMov++;
+            }
+            array[left] = temp;
+        }
+}
+template <class T>
+void binInsertionSort(T *array, int size) {
+    T temp;
+    int left;
+    int right;
+    int mid;
+    for (int i = 1;  i < size; i++)
+        if (array[i-1] > array[i]){
+            temp = array[i];
+            left = 0;
+            right = i-1;
+            do {
+                mid = (left + right) / 2;
+                if  (array[mid] < temp ) {
+                    left = mid + 1;
+                } else  {
+                    right = mid - 1;
+                }
+            } while (left <= right);
+            for (int j = i-1; j>=left; j--) {
+                array[j + 1] = array[j];
+            }
+            array[left] = temp;
+        }
 }
--- 5.7/MAIN5.7.cpp
+++ 5.7/MAIN5.7.cpp
@@ -48,9 +48,23 @@ void test(char sortType, T *array, int size) {
         comps = 0;
         movings = 0;
     }
+    if (sortType == '2') {
+        cout << "BINARY INSERTION SORT\n";
+        showArray(array, size);
+        binInsertionSort(array, comps, movings, size);
+        cout << endl;
+        showArray(array, size);
+        cout << endl;
+        cout << "Comparisons number: " << comps << endl;
+        cout << "Movings number: "<< movings << "\n";
+        cout << "-------------------------------------------------------------------------\n";
+        comps = 0;
+        movings = 0;
+    }
 }
 
 int main() {
+
     cout << "Please, enter N: ";
     int n;
     cin >> n;
@@ -60,45 +74,57 @@ int main() {
     int *ar1R = createArray<int>(n);
     int *ar2R = createArray<int>(n);
     int *ar3R = createArray<int>(n);
+    int *ar4R = createArray<int>(n);
     int *ar1A = createArray<int>(n);
     int *ar2A = createArray<int>(n);
     int *ar3A = createArray<int>(n);
+    int *ar4A= createArray<int>(n);
     int *ar1D = createArray<int>(n);
     int *ar2D = createArray<int>(n);
     int *ar3D = createArray<int>(n);
+    int *ar4D = createArray<int>(n);
 
-    //Заполнение 3х массивов 3мя типами
+    //Заполнение 4х массивов 4мя вариантами
     fillArrayRand(ar1R, n);
     fillArrayRand(ar2R, n);
     fillArrayRand(ar3R, n);
+    fillArrayRand(ar4R, n);
     fillArrayAsc(ar1A, n);
     fillArrayAsc(ar2A, n);
     fillArrayAsc(ar3A, n);
+    fillArrayAsc(ar4A, n);
     fillArrayDsc(ar1D, n);
     fillArrayDsc(ar2D, n);
     fillArrayDsc(ar3D, n);
+    fillArrayDsc(ar4D, n);
 
-    //Q - быстрая, B - пузырьком, I - вставками
+    //Q - быстрая, B - пузырьком, I - вставками, 2 - бинарными вставкавми
     test('Q', ar1R, n);
     test('B', ar2R, n);
     test('I', ar3R, n);
+    test('2', ar4R, n);
     cout << "\n\n\n";
     test('Q', ar1A, n);
     test('B', ar2A, n);
     test('I', ar3A, n);
+    test('2', ar4A, n);
     cout << "\n\n\n";
     test('Q', ar1D, n);
     test('B', ar2D, n);
     test('I', ar3D, n);
+    test('2', ar4D, n);
 
     deleteArray(ar1R);
     deleteArray(ar2R);
     deleteArray(ar3R);
+    deleteArray(ar4R);
     deleteArray(ar1A);
     deleteArray(ar2A);
     deleteArray(ar3A);
+    deleteArray(ar4A);
     deleteArray(ar1D);
     deleteArray(ar2D);
     deleteArray(ar3D);
+    deleteArray(ar4D);
     return 0;
 }
