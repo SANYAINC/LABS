7265e4374d32d3a6fcb2912b8f7b21f18235d6b3
SANYAINC <sanyaincorp@gmail.com>
2020-01-25 02:37:49 +0300

    Rework applied

--- 5.9/5.9.h
+++ 5.9/5.9.h
@@ -6,12 +6,21 @@ namespace v1 {
     bool findSolution(board board, int &arrangementsCou, int level = 0);
 }
 
+namespace v2 {
+    int solve(board board);
+    bool findSolution(board board, int &arrangementsNumber, int &bishopsCou, char color = 'W');
+}
+
 namespace v3 {
+    /*TODO убрать условие чтобы все цвета были заполнены, тк в задче подразумевается
+     * что свободные поля могут быть не под ударом. Главное чтобы ладей было 8 и они друг друга не били
+     */
     int solve(board board, int n = 92);
     bool findSolution(board board, int arrangementsNumber, int &arrangementsCou, int level = 0);
 }
 
-namespace v2 {
-    int solve(board board);
-    bool findSolution(board board, int &arrangementsNumber, int &bishopsCou, char color = 'W');
+namespace v4 {
+    /*TODO
+     * Аналог 3 варианта, но теперь ферзей будет 5, и каждое свободное поле должно быть под ударом
+     */
 }
