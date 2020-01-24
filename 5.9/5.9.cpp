a26d1c74208047dfde8fd46a668fb3d719b119ba
SANYAINC <sanyaincorp@gmail.com>
2020-01-24 13:52:09 +0300

    var 1 added

--- 5.9/5.9.cpp
+++ 5.9/5.9.cpp
@@ -2,6 +2,27 @@
 #include "5.9.h"
 using namespace std;
 
+bool v1::solve(board board, int &arrangementsCou, int level) {
+    if (level == 8) {
+        //cout << arrangementsCou + 1 << endl;
+        arrangementsCou++;
+        //board.show();
+        return true;
+    }
+    for (int j = 0; j < 8; ++j) {
+        if (board.field[level][j].isEmpty and board.field[level][j].underAttackTimes == 0) {
+            board.setUnit('R', level, j);
+            if (solve(board, arrangementsCou, level + 1)) {
+                board.removeUnit('R', level, j);
+                continue;
+            } else {
+                board.removeUnit('R', level, j);
+            }
+        }
+    }
+    return false;
+}
+
 void v3::transformNumber(int &n) {
     if (n > 92 or n < 1) {
         n = 92;
@@ -15,11 +36,11 @@ bool v3::solve(board board, int arrangementsNumber, int &arrangementsCou, int le
         }
         cout << arrangementsCou + 1 << endl;
         arrangementsCou++;
-        board.show('C');
+        board.show();
         return true;
     }
     for (int j = 0; j < 8; ++j) {
-        if (board.field[level][j].underAttackTimes == 0) {
+        if (board.field[level][j].isEmpty and board.field[level][j].underAttackTimes == 0) {
             board.setUnit('Q', level, j);
             if (solve(board, arrangementsNumber, arrangementsCou, level + 1)) {
                 board.removeUnit('Q', level, j);
--- 5.9/5.9.h
+++ 5.9/5.9.h
@@ -1,7 +1,12 @@
 #pragma once
 #include "class.h"
 
+namespace v1 {
+    bool solve(board board, int &arrangementsCou, int level = 0);
+}
+
 namespace v3 {
     void transformNumber(int &n);
     bool solve(board board, int arrangementsNumber, int &arrangementsCou, int level = 0);
 }
+
