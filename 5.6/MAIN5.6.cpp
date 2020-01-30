5c1294118924850be4e80ca1faae1125cf87a006
SANYAINC <sanyaincorp@gmail.com>
2020-01-30 16:34:34 +0300

    Ready for deploy

--- 5.6/MAIN5.6.cpp
+++ 5.6/MAIN5.6.cpp
@@ -8,10 +8,6 @@ int main() {
     double (*f1)(double, double, double) = &function;
 
     solveIntegralMR(f1); //Средние прямоугольники
-    cout << "------------------------------------------------------------\n";
-    solveIntegralT(f1); //Трапеции
-    cout << "------------------------------------------------------------\n";
-    solveIntegralS(f1); //Метод Симпсона
 
     return 0;
 }
