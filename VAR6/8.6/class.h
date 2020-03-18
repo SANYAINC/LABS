5edef4bc9a0465d08f354ed789b1aca9ea60b71d
SANYAINC <sanyaincorp@gmail.com>
2020-03-18 21:07:58 +0300

    Tree described

--- VAR6/8.6/class.h
+++ VAR6/8.6/class.h
@@ -1,2 +1,31 @@
 #pragma once
 
+template <class T>
+class tree {
+    class branch {
+    public:
+        T data;
+        branch* mLeftPtr;
+        branch* mRightPtr;
+
+        branch();
+    };
+
+    branch* root;
+    int mSize;
+
+public:
+
+    tree();
+    tree(T data);
+    tree(const tree& anotherTree);
+    ~tree();
+
+    void push(T data);
+    void removeLeaf(T data);
+    void find(T data);
+    void show();
+    void showLeaves();
+    void clear();
+
+};
