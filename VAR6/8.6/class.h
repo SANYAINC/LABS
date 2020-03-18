89f0998f2909701643dc06f73d369aa2aecb0ac4
SANYAINC <sanyaincorp@gmail.com>
2020-03-18 22:08:31 +0300

    "NLR" method added and implemented, "PushDataFrom" method added and implemented, Copy constructor implemented, "Push" method implemented

--- VAR6/8.6/class.h
+++ VAR6/8.6/class.h
@@ -14,6 +14,9 @@ class tree {
     branch* mRoot;
     int mSize;
 
+    void NLR(branch* curBranch, void(tree::*visit)(branch*));
+    void pushDataFrom(branch* curBranch);
+
 public:
 
     tree();
@@ -47,4 +50,55 @@ template <class T>
 tree <T>::tree(T data) {
     mRoot = new branch(data);
     mSize = 1;
+}
+
+template <class T>
+tree <T>::tree(const tree& anotherTree) {
+    mSize = 0;
+    mRoot = nullptr;
+    void(tree::*appendVisited)(branch*) = &pushDataFrom;
+    NLR(anotherTree.mRoot, appendVisited);
+}
+
+template <class T>
+void tree <T>::NLR(tree::branch* curBranch, void (tree::*visit)(branch*)) {
+    if (curBranch) {
+        (this->*visit)(curBranch);
+        NLR(curBranch->mLeftPtr, visit);
+        NLR(curBranch->mRightPtr, visit);
+    }
+}
+
+template <class T>
+void tree <T>::pushDataFrom(tree::branch* curBranch) {
+    push(curBranch->mData);
+}
+
+template <class T>
+void tree <T>::push(T data) {
+    mSize++;
+    if (mSize - 1 == 0) {
+        mRoot = new branch(data);
+        return;
+    }
+    branch* current = mRoot;
+    while (true) {
+        if (data > current->mData) {
+
+            if (!current->mRightPtr) {
+                current->mRightPtr = new branch(data);
+                return;
+            }
+
+            current = current->mRightPtr;
+        } else {
+
+            if (!current->mLeftPtr) {
+                current->mLeftPtr = new branch(data);
+                return;
+            }
+
+            current = current->mLeftPtr;
+        }
+    }
 }
