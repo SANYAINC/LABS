df1e432c7042d957565475ef1eba5caa79bdb279
SANYAINC <sanyaincorp@gmail.com>
2020-03-20 22:53:22 +0300

    Graph described

--- VAR6/8.7/class.h
+++ VAR6/8.7/class.h
@@ -1,2 +1,20 @@
 #pragma once
 #include "../8.4/class.h"
+
+
+class graph {
+    list <list <int>> adjacencyList;
+    int mSize;
+
+public:
+    graph();
+    graph(int vertices);
+    graph(const graph& anotherGraph);
+    ~graph();
+
+    void pushVertex();
+    void pushVertex(int pointToAmount, int* pointToVector, int pointedByAmount, int* pointedByVector);
+    void removeVertex(int index);
+    void show();
+    void clear();
+};
