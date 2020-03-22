4ea6d6510f6d64dfdccc04cc04cf0e2dc702da4d
SANYAINC <sanyaincorp@gmail.com>
2020-03-22 23:24:49 +0300

    Reworked

--- VAR6/8.7/source.cpp
+++ VAR6/8.7/source.cpp
@@ -3,11 +3,13 @@
 
 using namespace std;
 
+//В class.h подключен заголовок из лабы 8.4 (список) для построения списка смежности
+
 int main() {
 
     graph <char> g1(7);
 
-    g1.connect(1,4);
+    g1.connect(1,4); //Создает односторонний путь из первой вершины во вторую
     g1.connect(4,6);
     g1.connect(4,2);
     g1.connect(6,5);
@@ -18,7 +20,7 @@ int main() {
     g1.connect(3,2);
     g1.connect(2,0);
 
-    g1.show();
+    g1.show(); //Список смежности соответствует графу на graph.png
 
     g1.showLongestPath();
 
