67ef2de4803f0b06716b215f76421eedcd4dedde
SANYAINC <sanyaincorp@gmail.com>
2020-03-24 14:57:14 +0300

    Vector described

--- VAR6/8.8/vector.h
+++ VAR6/8.8/vector.h
@@ -1,1 +1,28 @@
 #pragma once
+#include <iostream>
+
+template <class T>
+class vector {
+    T* mVector;
+    int mSize;
+
+    bool validateIndex(int index) const;
+
+    friend std::ostream& operator<<(std::ostream& out, const vector& vectorToOut);
+
+public:
+    vector();
+    vector(int size, T filler);
+    vector(const vector& anotherVector);
+    ~vector();
+
+    T& operator[](int index) const;
+    vector& operator=(const vector& anotherVector);
+
+    vector& pushBack(T data);
+    vector& pushAt(int index, T data);
+    vector& deleteAt(int index);
+    vector& resize(int newSize, T filler);
+    void print() const;
+    int find(T data) const;
+};
