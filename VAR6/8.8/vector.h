9a82cb832da2a746ca209681368ddd39bfed7fda
SANYAINC <sanyaincorp@gmail.com>
2020-03-24 22:13:36 +0300

    Validator -> raiseException Resizer implemented.

--- VAR6/8.8/vector.h
+++ VAR6/8.8/vector.h
@@ -6,7 +6,7 @@ class vector {
     T* mVector;
     int mSize;
 
-    void validateIndex(int index) const;
+    void raiseException(int type, int value) const;
 
     template <class V>
     friend std::ostream& operator<<(std::ostream& out, const vector<V>& vectorToOut);
@@ -20,10 +20,10 @@ public:
     T& operator[](int index) const;
     vector& operator=(const vector& anotherVector);
 
+    vector& resize(int newSize);
     vector& pushBack(T data);
     vector& pushAt(int index, T data);
     vector& deleteAt(int index);
-    vector& resize(int newSize, T filler);
     void print() const;
     int find(T data) const;
 };
@@ -58,10 +58,21 @@ vector <T>::~vector() {
 }
 
 template <class T>
-void vector <T>::validateIndex(const int index) const {
-    if (index < 0 or index >= mSize) {
-        std::cerr << "INDEX " << index << " IS OUT OF RANGE (0 - " << mSize - 1 << ")" << std::endl;
-        exit(404);
+void vector <T>::raiseException(const int type, const int value) const {
+    switch (type) {
+        case 900: {
+            std::cerr << "INDEX " << value << " IS OUT OF RANGE (0 - " << mSize - 1 << ")" << std::endl;
+            exit(type);
+        }
+        case 901: {
+            std::cerr << "CAN'T RESIZE TO " << value << std::endl;
+            exit(type);
+        }
+
+
+        default: {
+            return;
+        }
     }
 }
 
@@ -84,7 +95,9 @@ std::ostream& operator<<(std::ostream& out, const vector<T>& vectorToOut) {
 
 template <class T>
 T& vector <T>::operator[](const int index) const {
-    validateIndex(index);
+    if (index < 0 or index >= mSize) {
+        raiseException(900, index);
+    }
     return mVector[index];
 }
 
@@ -100,4 +113,37 @@ vector<T>& vector <T>::operator=(const vector& anotherVector) {
         mVector[i] = anotherVector.mVector[i];
     }
     return *this;
+}
+
+template <class T>
+vector<T>& vector <T>::resize(const int newSize) {
+    if (newSize < 0) {
+        raiseException(901, newSize);
+    }
+    if (newSize == 0) {
+        delete mVector;
+        mVector = nullptr;
+        mSize = 0;
+        return *this;
+    }
+    if (newSize == mSize) {
+        return *this;
+    }
+    T *newVector = new T[newSize];
+    int index = 0;
+    if (newSize > mSize) {
+        for (; index < mSize; ++index) {
+            newVector[index] = mVector[index];
+        }
+        for (; index < newSize; ++index) {
+            newVector[index] = mVector[0];
+        }
+    } else {
+        for (; index < newSize; ++index) {
+            newVector[index] = mVector[index];
+        }
+    }
+    delete [] mVector;
+    mVector = newVector;
+    mSize = newSize;
 }
