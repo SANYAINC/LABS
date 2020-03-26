b1a26587f61029d9b41994798f00ea607d1378cb
SANYAINC <sanyaincorp@gmail.com>
2020-03-26 12:17:37 +0300

    Minor changes

--- VAR6/8.8/vector.h
+++ VAR6/8.8/vector.h
@@ -6,7 +6,7 @@ class vector {
     T* mVector;
     int mSize;
 
-    void raiseException(int type, int value) const;
+    void raise(int type, int value) const;
 
     template <class V>
     friend std::ostream& operator<<(std::ostream& out, const vector<V>& vectorToOut);
@@ -20,7 +20,7 @@ public:
     T& operator[](int index) const;
     vector& operator=(const vector& anotherVector);
 
-    vector& resize(int newSize);
+    vector& resize(int newSize, T filler);
     vector& pushBack(T data);
     vector& pushAt(int pos, T data);
     vector& deleteAt(int pos);
@@ -59,7 +59,7 @@ vector <T>::~vector() {
 }
 
 template <class T>
-void vector <T>::raiseException(const int type, const int value) const {
+void vector <T>::raise(const int type, const int value) const {
     switch (type) {
         case 900: {
             std::cerr << "INDEX " << value << " IS OUT OF RANGE (0 - " << mSize - 1 << ")" << std::endl;
@@ -97,7 +97,7 @@ std::ostream& operator<<(std::ostream& out, const vector<T>& vectorToOut) {
 template <class T>
 T& vector <T>::operator[](const int index) const {
     if (index < 0 or index >= mSize) {
-        raiseException(900, index);
+        raise(900, index);
     }
     return mVector[index];
 }
@@ -117,9 +117,12 @@ vector<T>& vector <T>::operator=(const vector& anotherVector) {
 }
 
 template <class T>
-vector<T>& vector <T>::resize(const int newSize) {
+vector<T>& vector <T>::resize(const int newSize, T filler) {
     if (newSize < 0) {
-        raiseException(901, newSize);
+        raise(901, newSize);
+    }
+    if (newSize == mSize) {
+        return *this;
     }
     if (newSize == 0) {
         delete mVector;
@@ -127,21 +130,13 @@ vector<T>& vector <T>::resize(const int newSize) {
         mSize = 0;
         return *this;
     }
-    if (newSize == mSize) {
-        return *this;
-    }
     T *newVector = new T[newSize];
     int index = 0;
-    if (newSize > mSize) {
-        for (; index < mSize; ++index) {
+    for (; index < newSize; ++index) {
+        if (index < mSize) {
             newVector[index] = mVector[index];
-        }
-        for (; index < newSize; ++index) {
-            newVector[index] = mVector[0];
-        }
-    } else {
-        for (; index < newSize; ++index) {
-            newVector[index] = mVector[index];
+        } else {
+            newVector[index] = filler;
         }
     }
     delete [] mVector;
@@ -152,45 +147,46 @@ vector<T>& vector <T>::resize(const int newSize) {
 
 template <class T>
 vector<T>& vector <T>::pushBack(T data) {
-    resize(mSize+1);
-    mVector[mSize-1] = data;
+    resize(mSize+1, data);
     return *this;
 }
 
 template <class T>
 vector<T>& vector <T>::pushAt(int pos, T data) {
     if (pos < 0 or pos > mSize) {
-        raiseException(900, pos);
+        raise(900, pos);
     }
     if (pos == mSize) {
         pushBack(data);
         return *this;
     }
-    T* newVector = new T[++mSize];
+    T* newVector = new T[mSize + 1];
     int index = 0;
     for (; index < pos; ++index) {
         newVector[index] = mVector[index];
     }
     newVector[pos] = data;
     index++;
-    for (; index < mSize; ++index) {
+    for (; index < mSize + 1; ++index) {
         newVector[index] = mVector[index - 1];
     }
     delete [] mVector;
     mVector = newVector;
+    mSize++;
     return *this;
 }
 
 template <class T>
 vector<T>& vector <T>::deleteAt(int pos) {
     if (pos < 0 or pos >= mSize) {
-        raiseException(900, pos);
+        raise(900, pos);
     }
     if (mSize == 1) {
         mSize = 0;
         mVector = nullptr;
+        return *this;
     }
-    T *newVector = new T[--mSize];
+    T *newVector = new T[mSize - 1];
     int index = 0;
     for (; index < pos; ++index) {
         newVector[index] = mVector[index];
@@ -201,14 +197,16 @@ vector<T>& vector <T>::deleteAt(int pos) {
     }
     delete [] mVector;
     mVector = newVector;
+    mSize--;
     return *this;
 }
 
 template <class T>
-vector<T>& vector <T>::clear(int index) {
+vector<T>& vector <T>::clear() {
     delete [] mVector;
     mVector = nullptr;
     mSize = 0;
+    return *this;
 }
 
 template <class T>
