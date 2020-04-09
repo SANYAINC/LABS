4719a311f1fd6eb57b2e0aba2d250cb8c7597f71
SANYAINC <sanyaincorp@gmail.com>
2020-04-09 19:39:24 +0300

    Ready for deploy

--- VAR6/9.3/source.cpp
+++ VAR6/9.3/source.cpp
@@ -1,17 +1,11 @@
 #include <iostream>
 #include "expression.h"
 
-#define MAX_CHAR 100
 
 using namespace std;
 
 int main() {
-    char exprInit[] = " 6 +(5.45-2.45) *2- 3/(7.3 + 4.7) ";
-    char exprKbrd[MAX_CHAR];
-    cout << "Init expression: " << exprInit << endl;
-    cout << "Please, enter your expression: ";
-    cin.getline(exprKbrd, MAX_CHAR);
-
+    char exprInit[] = " 6 +(5.45*3-2.45) *2- 3/(7.3 + 4.7-2)";
 
     expression expr(exprInit);
 
@@ -21,13 +15,6 @@ int main() {
     expr.convertToRPN();
 
     cout << expr.calculate() << endl << endl;
-
-    expression expr2(exprKbrd);
-    cout << "Your expression" << endl;
-    expr2.show();
-    cout << endl;
-    expr2.convertToRPN();
-    cout << expr2.calculate() << endl;
 
 
     system("pause");
