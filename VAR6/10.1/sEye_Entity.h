dece79278d90aa4f02f3db4525ae53208582d0d5
SANYAINC <sanyaincorp@gmail.com>
2020-04-12 15:28:45 +0300

    Class "Entity" described

--- VAR6/10.1/sEye_Entity.h
+++ VAR6/10.1/sEye_Entity.h
@@ -1,1 +1,24 @@
 #pragma once
+
+class entity {
+protected:
+    short int mXMin;
+    short int mXMax;
+    short int mYMin;
+    short int mYMax;
+
+    entity(short int xMin, short int xMax, short int yMin, short int yMax);
+
+public:
+    short int getXMin() const;
+    short int getXMax() const;
+    short int getYMin() const;
+    short int getYMax() const;
+
+    bool setXMin(short int xMin) const;
+    bool setXMax(short int xMax) const;
+    bool setYMin(short int yMin) const;
+    bool setYMax(short int yMax) const;
+
+    virtual void print() const = 0;
+};
