85ed0327b9edcb9e9010fde3e5016258a683e62f
SANYAINC <sanyaincorp@gmail.com>
2020-04-12 15:57:24 +0300

    Class "Human" described

--- VAR6/10.1/sEye_Entity.h
+++ VAR6/10.1/sEye_Entity.h
@@ -22,3 +22,26 @@ public:
 
     virtual void print() const = 0;
 };
+
+
+class human: public entity {
+protected:
+    char mSex;
+    char mIsAdult;
+    char mHasGlasses;
+    char mHasBeard;
+public:
+    human(short int xMin, short int xMax, short int yMin, short int yMax);
+
+    char getSex() const;
+    char getAge() const;
+    char getGlasses() const;
+    char getBeard() const;
+
+    void setSex(char sex);
+    void setAge(char isAdult);
+    void setGlasses(char hasGlasses);
+    void setBeard(char hasBeard);
+
+    void print() const override;
+};
