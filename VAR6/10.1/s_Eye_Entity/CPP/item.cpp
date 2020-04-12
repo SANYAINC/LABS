f9d99162c0f5b4b2ffe6f4908b457df357354138
SANYAINC <sanyaincorp@gmail.com>
2020-04-13 01:47:38 +0300

    Class "Item" implemented

---
+++ VAR6/10.1/s_Eye_Entity/CPP/item.cpp
@@ -0,0 +1,188 @@
+#include "../sEye_Entity.h"
+
+#include <iostream>
+
+item::item(short int xMin, short int xMax, short int yMin, short int yMax,
+            const char* name, const char* timeWhenAppeared) : entity(xMin, xMax, yMin, yMax) {
+
+    validate(name);
+    validate(timeWhenAppeared);
+
+    int nameSize = 0;
+    for (;name[nameSize++];);
+    int timeSize = 0;
+    for (;timeWhenAppeared[timeSize++];);
+
+    int newNameSize = nameSize < MAX_CHAR ? nameSize : MAX_CHAR;
+    int newTimeSize = timeSize < MAX_CHAR ? timeSize : MAX_CHAR;
+
+    mName = new char[newNameSize];
+    mFirstAppearanceTime = new char[newTimeSize];
+
+    int i = 0;
+    for (; i < newNameSize - 1; ++i) {
+        mName[i] = name[i];
+    }
+    mName[i] = 0;
+
+    i = 0;
+    for (; i < newTimeSize - 1; ++i) {
+        mFirstAppearanceTime[i] = timeWhenAppeared[i];
+    }
+    mFirstAppearanceTime[i] = 0;
+
+}
+
+item::item(const item& anotherItem)
+          : entity(anotherItem.mXMin, anotherItem.mXMax, anotherItem.mYMin, anotherItem.mYMax) {
+
+    int anotherNameSize = 0;
+    for (;anotherItem.mName[anotherNameSize++];);
+    int anotherTimeSize = 0;
+    for (;anotherItem.mFirstAppearanceTime[anotherTimeSize++];);
+
+    mName = new char[anotherNameSize];
+    mFirstAppearanceTime = new char[anotherTimeSize];
+
+    int i = 0;
+    while (i < anotherNameSize - 1) {
+        mName[i] = anotherItem.mName[i];
+        ++i;
+    }
+    mName[i] = 0;
+
+    i = 0;
+    while (i < anotherTimeSize - 1) {
+        mFirstAppearanceTime[i] = anotherItem.mFirstAppearanceTime[i];
+        ++i;
+    }
+    mFirstAppearanceTime[i] = 0;
+}
+
+item& item::operator=(const item& anotherItem) {
+    if (this == &anotherItem) {
+        return *this;
+    }
+
+    mXMin = anotherItem.mXMin;
+    mXMax = anotherItem.mXMax;
+    mYMin = anotherItem.mYMin;
+    mYMax = anotherItem.mYMax;
+
+    int anotherSize = 0;
+    for (;anotherItem.mName[anotherSize++];);
+    int currentSize = 0;
+    for (;mName[currentSize++];);
+
+    if (currentSize < anotherSize) {
+        delete [] mName;
+        mName = new char[anotherSize];
+    }
+
+    int i = 0;
+    for (; i < anotherSize - 1; ++i) {
+        mName[i] = anotherItem.mName[i];
+    }
+    mName[i] = 0;
+
+
+    anotherSize = 0;
+    for (;anotherItem.mFirstAppearanceTime[anotherSize++];);
+    currentSize = 0;
+    for (;mFirstAppearanceTime[currentSize++];);
+
+    if (currentSize < anotherSize) {
+        delete [] mFirstAppearanceTime;
+        mFirstAppearanceTime = new char[anotherSize];
+    }
+
+    i = 0;
+    for (; i < anotherSize - 1; ++i) {
+        mFirstAppearanceTime[i] = anotherItem.mFirstAppearanceTime[i];
+    }
+    mFirstAppearanceTime[i] = 0;
+
+    return *this;
+}
+
+item::~item() {
+    delete [] mName;
+    delete [] mFirstAppearanceTime;
+}
+
+void item::validate(const char* string) {
+    try {
+        if (string[0] == 0) {
+            throw "STRING IS EMPTY";
+        }
+    } catch (const char* errorMessage) {
+        std::cerr << errorMessage << std::endl;
+        exit(1004);
+    }
+}
+
+const char* item::getName() const {
+    return mName;
+}
+
+const char* item::getTime() const {
+    return mFirstAppearanceTime;
+}
+
+void item::setName(const char* name) {
+    validate(name);
+
+    int anotherNameSize = 0;
+    for (;name[anotherNameSize++];);
+    if (anotherNameSize >= MAX_CHAR) {
+        anotherNameSize = MAX_CHAR;
+    }
+
+    int currentNameSize = 0;
+    for (;mName[currentNameSize++];);
+
+    if (currentNameSize < anotherNameSize) {
+        delete [] mName;
+        mName = new char[anotherNameSize];
+    }
+
+    int i = 0;
+    for (; i < anotherNameSize - 1; ++i) {
+        mName[i] = name[i];
+    }
+    mName[i] = 0;
+}
+
+void item::setTime(const char* firstAppearanceTime) {
+    validate(firstAppearanceTime);
+
+    int anotherTimeSize = 0;
+    for (;firstAppearanceTime[anotherTimeSize++];);
+    if (anotherTimeSize >= MAX_CHAR) {
+        anotherTimeSize = MAX_CHAR;
+    }
+
+    int currentTimeSize = 0;
+    for (;mFirstAppearanceTime[currentTimeSize++];);
+
+    if (currentTimeSize < anotherTimeSize) {
+        delete [] mFirstAppearanceTime;
+        mFirstAppearanceTime = new char[anotherTimeSize];
+    }
+
+    int i = 0;
+    for (; i < anotherTimeSize - 1; ++i) {
+        mFirstAppearanceTime[i] = firstAppearanceTime[i];
+    }
+    mFirstAppearanceTime[i] = 0;
+}
+
+void item::print() const {
+    std::cout << "{ " << "\"Obj\":" << std::endl;
+    std::cout << "\t\"Frame area\": [[" << mXMin << ", " << mYMax << "], [" << mXMax << ", " << mYMin << "]]," << std::endl;
+    std::cout << "\t\"Item\": {" << std::endl;
+    std::cout << "\t\t\"Name\": " << "\"" << mName <<"\"," << std::endl;
+    std::cout << "\t\t\"FirstAppearedTime\": " << "\"" << mFirstAppearanceTime <<"\"" << std::endl;
+    std::cout << "\t}" << std::endl;
+    std::cout << "}" << std::endl;
+}
