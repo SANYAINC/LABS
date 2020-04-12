c2359b55e0969e194088c4b498c1fcd6bce4d881
SANYAINC <sanyaincorp@gmail.com>
2020-04-12 18:17:05 +0300

    Class "Entity" implemented

---
+++ VAR6/10.1/s_Eye_Entity/CPP/entity.cpp
@@ -0,0 +1,55 @@
+#include "../sEye_Entity.h"
+
+#include <iostream>
+
+entity::entity(short int xMin, short int xMax, short int yMin, short int yMax) {
+    validateRange(xMin, xMax, 'X');
+    validateRange(yMin, yMax, 'Y');
+
+    mXMin = xMin;
+    mXMax = xMax;
+    mYMin = yMin;
+    mYMax = yMax;
+}
+
+void entity::validateRange(short int min, short int max, char axis) {
+    try {
+        if (min >= max) {
+            throw "MIN COORDINATE IS HIGHER THAN MAX";
+        }
+        if (min < 0 or max >= (axis == 'X' ? FRAME_WIDTH : FRAME_HEIGHT)) {
+            throw "COORDINATES ARE OUT OF RANGE";
+        }
+    } catch (const char* errorMessage) {
+        std::cerr << errorMessage << std::endl;
+        exit(1001);
+    }
+}
+
+short int entity::getXMin() const {
+    return mXMin;
+}
+
+short int entity::getXMax() const {
+    return mXMax;
+}
+
+short int entity::getYMin() const {
+    return mYMin;
+}
+
+short int entity::getYMax() const {
+    return mYMax;
+}
+
+void entity::setXRange(short int xMin, short int xMax) {
+    validateRange(xMin, xMax, 'X');
+    mXMin = xMin;
+    mXMax = xMax;
+}
+
+void entity::setYRange(short int yMin, short int yMax) {
+    validateRange(yMin, yMax, 'Y');
+    mYMin = yMin;
+    mYMax = yMax;
+}
